d7e7d4cf5f4de309f224338f9ad5fd3ee6c3c958
cedi23 <cedric.weigel@outlook.de>
2023-10-27 15:22:21 +0200

    Navbar and Contact me section changes

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-// scripts.js
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+    e.preventDefault();
 
-document.getElementById('contact-form').addEventListener('submit', function (e) {
-    e.preventDefault(); // Prevent the default form submission behavior
+    var name = document.getElementById("name").value;
+    var message = document.getElementById("message").value;
 
-    // Get the form values
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
+    var emailBody = "Name: " + name + "\n\n" + "Message:\n" + message;
 
-    // You can now use these values to send the data to a server or perform other actions.
-    console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
+    var mailtoLink = "mailto:cedric.weigel7@gmail.com" +
+        "?subject=Message from " + name +
+        "&body=" + encodeURIComponent(emailBody);
 
-    // Get the current year and update the year in the copyright notice
-    document.getElementById("year").textContent = new Date().getFullYear();
+    window.location.href = mailtoLink;
+});
+
+
